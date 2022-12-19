6fb3e060ca2117f5bb629dd9eed2a4ae09983c83
GithubSre <sreeniketh.cheruvu2010@gmail.com>
2022-12-19 20:27:21 +0530

    code

---
+++ main.py
@@ -0,0 +1,24 @@
+from MotorModule import Motor
+import KeyboardModule as kb
+
+#################################################################################
+motor = Motor(2, 3, 4, 22, 17, 27)
+#################################################################################
+
+kb.init()
+
+def main():
+    if kb.getKey('UP'):
+        motor.move(0.6, 0, 1)
+    elif kb.getKey('DOWN'):
+        motor.move(-0.6, 0, 1)
+    elif kb.getKey('LEFT'):
+        motor.move(0.5, 0.6, 1)
+    elif kb.getKey('RIGHT'):
+        motor.move(0.5, -0.6, 1)
+    else:
+        motor.stop()
+
+if __name__ == '__main__':
+    while True:
+        main()
